8cd52e2599f3ac430f5d8cb7e02a19510ed68785
PyrovSergey <pyrov.sergey@gmail.com>
2018-03-03 12:18:10 +0300

    update_15

--- app/src/main/java/com/test/projectnewsudacity/MainActivity.java
+++ app/src/main/java/com/test/projectnewsudacity/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -16,7 +17,7 @@ import android.preference.PreferenceManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,14 +30,23 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>>, SwipeRefreshLayout.OnRefreshListener {
 
+    private static final String PAGE_SIZE = "page-size";
+    private static final String API_KEY = "api-key";
+    private static final String KEY = "test";
+    private static final String SHOW_FIELDS = "show-fields";
+    private static final String THUMBNAIL_TRAIL_TEXT_BYLINE = "thumbnail,trailText,byline";
+    private static final String NONE = "none";
+    private static final String SECTION = "section";
+    private static final String ORDER_BY = "order-by";
+    private static final String NEWEST = "newest";
+    private static final String RELEVANCE = "relevance";
+    private static final String QUERY = "q";
+    private static final String HTTPS_PLAY_GOOGLE_COM_STORE_APPS_DETAILS_ID_COM_ANDROID_CHROME = "https://play.google.com/store/apps/details?id=com.android.chrome";
     private NewsAdapter newsAdapter;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private NewsLoader loader;
 
     private static final String USGS_REQUEST_URL = "http://content.guardianapis.com/search";
-    //private static final String STR_1 = "http://content.guardianapis.com/search?order-by=newest&page-size=200&q=";
-    ///private static final String STR_3 = "&api-key=test&order-by=newest&show-fields=thumbnail,trailText,byline";
-
     private static final int NEWS_LOADER_ID = 1;
 
     @Override
@@ -63,21 +73,26 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 News currentNews = newsAdapter.getItem(i);
                 Uri newsUri = Uri.parse(currentNews.getUrl());
+                PackageManager packageManager = MainActivity.this.getPackageManager();
                 Intent newsIntent = new Intent(Intent.ACTION_VIEW, newsUri);
-                startActivity(newsIntent);
+                if (newsIntent.resolveActivity(packageManager) != null) {
+                    startActivity(newsIntent);
+                } else {
+                    Uri googlePlay = Uri.parse(HTTPS_PLAY_GOOGLE_COM_STORE_APPS_DETAILS_ID_COM_ANDROID_CHROME);
+                    Intent intentGooglePlay = new Intent(Intent.ACTION_VIEW, googlePlay);
+                    startActivity(intentGooglePlay);
+                    Toast.makeText(getBaseContext(), R.string.install_the_browser, Toast.LENGTH_LONG).show();
+                }
             }
         });
 
-        Log.e("MyTAGS", "сработал метод initLoader()");
         loader = (NewsLoader) getLoaderManager().initLoader(NEWS_LOADER_ID, null, this);
     }
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
-        Log.e("MyTAGS", "сработал метод onCreateLoader()");
         if (i == NEWS_LOADER_ID) {
             loader = new NewsLoader(this, searchResult(null));
-            //Log.e("MyTAGS", result);
         }
         return loader;
     }
@@ -88,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 .setMessage(message)
                 .setIcon(icon)
                 .setCancelable(false)
-                .setNegativeButton("ОК",
+                .setNegativeButton(R.string.ok,
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 dialog.cancel();
@@ -109,13 +124,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             mSwipeRefreshLayout.setRefreshing(false);
         }
         mSwipeRefreshLayout.setRefreshing(false);
-        Log.e("MyTAGS", "сработал метод onLoadFinished()");
     }
 
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
         newsAdapter.clear();
-        Log.e("MyTAGS", "сработал метод onLoaderReset()");
     }
 
     @Override
@@ -133,7 +146,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onRefresh() {
-        Log.e("MyTAGS", "сработал метод onRefresh()");
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
@@ -152,8 +164,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         getMenuInflater().inflate(R.menu.main, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) searchItem.getActionView();
-
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
         if (null != searchManager) {
             searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         }
@@ -162,7 +174,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String inputQuery) {
-                // тут получаем строку для поиска
                 mSwipeRefreshLayout.setRefreshing(true);
                 ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
@@ -188,29 +199,31 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         return true;
     }
 
-    // http://content.guardianapis.com/search?order-by=newest&page-size=200&q=&api-key=test&order-by=newest&show-fields=thumbnail,trailText,byline
     private String searchResult(String query) {
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String pageSize = sharedPreferences.getString(getString(R.string.settings_page_size_key), getString(R.string.settings_page_size_default));
 
         Uri baseUri = Uri.parse(USGS_REQUEST_URL);
         Uri.Builder uriBuilder = baseUri.buildUpon();
 
-        uriBuilder.appendQueryParameter("page-size", pageSize);                         // можем менять кол-во строк
-        uriBuilder.appendQueryParameter("api-key", "test");
-        uriBuilder.appendQueryParameter("show-fields", "thumbnail,trailText,byline");
+        String pageSize = sharedPreferences.getString(getString(R.string.settings_page_size_key), getString(R.string.settings_page_size_default));
+        if (TextUtils.isEmpty(pageSize)) {
+            pageSize = "0";
+        }
+        uriBuilder.appendQueryParameter(PAGE_SIZE, pageSize);
+        uriBuilder.appendQueryParameter(API_KEY, KEY);
+        uriBuilder.appendQueryParameter(SHOW_FIELDS, THUMBNAIL_TRAIL_TEXT_BYLINE);
         String section = sharedPreferences.getString(getString(R.string.settings_only_show_key), getString(R.string.settings_only_show_default));
-        if (!section.equals("none")) {
-            uriBuilder.appendQueryParameter("section", section);   ///
+        if (!section.equals(NONE)) {
+            uriBuilder.appendQueryParameter(SECTION, section);
         }
 
         if (query == null) {
-            uriBuilder.appendQueryParameter("order-by", "newest");
+            uriBuilder.appendQueryParameter(ORDER_BY, NEWEST);
             return uriBuilder.toString();
         }
-        uriBuilder.appendQueryParameter("order-by", "relevance");
-        uriBuilder.appendQueryParameter("q", query);
+        uriBuilder.appendQueryParameter(ORDER_BY, RELEVANCE);
+        uriBuilder.appendQueryParameter(QUERY, query);
 
         return uriBuilder.toString();
     }
--- app/src/main/java/com/test/projectnewsudacity/QueryUtils.java
+++ app/src/main/java/com/test/projectnewsudacity/QueryUtils.java
@@ -2,7 +2,6 @@ package com.test.projectnewsudacity;
 
 import android.text.Html;
 import android.text.TextUtils;
-import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,6 +20,21 @@ import java.util.List;
 
 public final class QueryUtils {
 
+    private static final String RESPONSE = "response";
+    private static final String RESULTS = "results";
+    private static final String WEB_TITLE = "webTitle";
+    private static final String TITLE_IS_MISSING = "Title is missing";
+    private static final String SECTION_NAME = "sectionName";
+    private static final String SECTION = "section";
+    private static final String WEB_URL = "webUrl";
+    private static final String DEFAULT_LINK = "https://www.theguardian.com";
+    private static final String WEB_PUBLICATION_DATE = "webPublicationDate";
+    private static final String DATE = "date";
+    private static final String FIELDS = "fields";
+    private static final String BYLINE = "byline";
+    private static final String TRAIL_TEXT = "trailText";
+    private static final String THUMBNAIL = "thumbnail";
+    private static final String GET = "GET";
     public static boolean isAnyNews;
 
     private QueryUtils() {
@@ -28,7 +42,6 @@ public final class QueryUtils {
 
     public static List<News> fetchNewsData(String requestUrl) {
         URL url = createUrl(requestUrl);
-
         String jsonResponce = null;
         try {
             jsonResponce = makeHttpRequest(url);
@@ -48,8 +61,8 @@ public final class QueryUtils {
         List<News> news = new ArrayList<>();
         try {
             JSONObject jsonObject = new JSONObject(newsRequestJSON);
-            JSONObject response = jsonObject.getJSONObject("response");
-            JSONArray results = response.getJSONArray("results");
+            JSONObject response = jsonObject.getJSONObject(RESPONSE);
+            JSONArray results = response.getJSONArray(RESULTS);
             if (results.length() != 0) {
                 isAnyNews = true;
             } else {
@@ -68,57 +81,55 @@ public final class QueryUtils {
                     continue;
                 }
 
-                title = currentNews.optString("webTitle");
+                title = currentNews.optString(WEB_TITLE);
                 if (TextUtils.isEmpty(title)) {
-                    title = "Title is missing";
+                    title = TITLE_IS_MISSING;
                 }
                 if (title.contains("|")) {
                     int index = title.indexOf("|");
                     title = title.substring(0, index - 1);
                 }
 
-                sectionName = currentNews.optString("sectionName");
+                sectionName = currentNews.optString(SECTION_NAME);
                 if (TextUtils.isEmpty(sectionName)) {
-                    sectionName = "section";
+                    sectionName = SECTION;
                 }
 
-                url = currentNews.optString("webUrl");
+                url = currentNews.optString(WEB_URL);
                 if (TextUtils.isEmpty(url)) {
-                    url = "https://www.theguardian.com";
+                    url = DEFAULT_LINK;
                 }
 
-                String resultDate = currentNews.optString("webPublicationDate");
+                String resultDate = currentNews.optString(WEB_PUBLICATION_DATE);
                 if (TextUtils.isEmpty(resultDate)) {
-                    date = "date";
+                    date = DATE;
                 } else {
                     date = resultDate.substring(11, 16) + "    " + resultDate.substring(0, 10);
                     date = date.replaceAll("-", ".");
                 }
 
-                JSONObject fields = currentNews.optJSONObject("fields");
+                JSONObject fields = currentNews.optJSONObject(FIELDS);
                 if (fields == null) {
                     continue;
                 }
-                byline = fields.optString("byline");
+
+                byline = fields.optString(BYLINE);
                 if (TextUtils.isEmpty(byline)) {
                     byline = "";
                 }
 
-                trailText = fields.optString("trailText");
+                trailText = fields.optString(TRAIL_TEXT);
                 if (TextUtils.isEmpty(byline)) {
-                    trailText = " ";
+                    trailText = "";
                 }
+
                 trailText = stripHtml(trailText);
 
-                thumbnail = fields.optString("thumbnail");
-                //Log.e("MyPARSE", thumbnail);
+                thumbnail = fields.optString(THUMBNAIL);
                 if (TextUtils.isEmpty(byline)) {
-                    Log.e("MyPARSE", thumbnail);
-                    trailText = "https://media.guim.co.uk/04c27033759a6231874e6b6bf2aa67e83550050e/0_0_875_525/500.jpg";
+                    trailText = null;
                 }
-
                 news.add(new News(title, sectionName, date, url, byline, trailText, thumbnail));
-
             }
         } catch (JSONException e) {
 
@@ -137,7 +148,7 @@ public final class QueryUtils {
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setReadTimeout(10000 /* milliseconds */);
             urlConnection.setConnectTimeout(15000 /* milliseconds */);
-            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestMethod(GET);
             urlConnection.connect();
 
             if (urlConnection.getResponseCode() == 200) {
